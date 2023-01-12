16b95aee382db40527830924794ab4e38d9d17b7
Cuong Nguyen 2k1 <nguyenvancuong19032002@gmail.com>
2023-01-12 11:58:47 +0700

    Clone Youtube with React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+function App() {
+    return (
+        <Router>
+           <div>YOUTUBE COMMINGS...</div>
+        </Router>
+    );
+}
+
+export default App;
